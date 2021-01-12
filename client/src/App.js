32e0693b34abb4c9d7047e26fbda00b80db3d117
Matt Steiner <Metropolis.4488@gmail.com>
2021-01-11 21:21:33 -0500

    react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Order from "./pages/Order";
+import Manage from "./pages/Manage";
+import NavTabs from "./components/NavTabs";
+import Header from "./components/Header";
 
-
-function App() {}
+function App() {
+    return (
+        <Router>
+            <div>
+            <Header />
+            <NavTabs />
+            <Route exact path="/" component={Order} />
+            <Route exact path="/manage"  component={Manage} />
+            </div>
+        </Router>
+    )
+}
 
 export default App;
---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs() {
+    const location = useLocation();
+
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Order
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/manage" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Manage
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavTabs;
