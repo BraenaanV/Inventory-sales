81352890d7572ef4dfd01f92c2f9cc741423e283
BraenaanV <braenaan.vaughan@gmail.com>
2021-01-13 13:06:09 -0500

    added stripe and payment tab, working on fixing an HTML routing issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Order from "./pages/Order";
 import Manage from "./pages/Manage";
+import Stripe from "./pages/StripeContainer"
 import NavTabs from "./components/NavTabs";
 import Header from "./components/Header";
 
@@ -13,6 +14,7 @@ function App() {
             <NavTabs />
             <Route exact path="/" component={Order} />
             <Route exact path="/manage"  component={Manage} />
+            <Route exact path="/stripeContainer" component={Stripe} />
             </div>
         </Router>
     )
